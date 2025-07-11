866b59dcc215f40078a833315e81ef6b47b25721
nak-0 <skrudtk40@gmail.com>
2025-07-11 15:02:47 +0900

    Add files via upload

---
+++ project/b_m/js/main.js
@@ -0,0 +1,30 @@
+$(function () {
+
+  const s_banner = new Swiper(".s_banner", {
+    autoplay: {
+      delay: 3000, // 슬라이드가 머무르는 시간, 5000=5초
+      disableOnInteraction: false, // 스와이프 후 자동 재생이 비활성화 되지 않음
+    },
+    speed: 1000,
+    loop: true,
+
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar", // 버튼 종류 설정 'bullets' | 'fraction' | 'progressbar'
+      clickable: true, // 버튼 클릭 여부
+    },
+  });
+
+  const e_card = new Swiper(".e_card", {
+    autoplay: {
+      delay: 3000, // 슬라이드가 머무르는 시간, 5000=5초
+      disableOnInteraction: false, // 스와이프 후 자동 재생이 비활성화 되지 않음
+    },
+    speed: 1000,
+    loop: true,
+  });
+
+
+
+});
+/* 종료 */
